86497b6cb05cb3325aebe23138bc3243ebdc6fe9
Sednev Michail <sednevmxail@gmailc.om>
2015-09-09 01:24:03 +0400

    Simple video play_back

--- app/src/main/java/com/miiskin/videolibraryproject/ui/video/preview/VideoPreviewFragment.java
+++ app/src/main/java/com/miiskin/videolibraryproject/ui/video/preview/VideoPreviewFragment.java
@@ -1,12 +1,19 @@
 package com.miiskin.videolibraryproject.ui.video.preview;
 
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.miiskin.videolibraryproject.R;
@@ -15,19 +22,33 @@ import com.miiskin.videolibraryproject.content.loader.VideoInfoLoader;
 import com.miiskin.videolibraryproject.ui.AbstractFragment;
 import com.miiskin.videolibraryproject.ui.video.list.VideoLibraryActivity;
 
+import java.io.IOException;
+
 import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by Newshka on 08.09.2015.
  */
-public class VideoPreviewFragment extends AbstractFragment implements LoaderManager.LoaderCallbacks<VideoInfo> {
+public class VideoPreviewFragment extends AbstractFragment implements LoaderManager.LoaderCallbacks<VideoInfo>, SurfaceHolder.Callback{
 
     private static final String EXTRA_VIDEO_ID = "video_id";
     private int mVideoId;
     private VideoInfo mVideoInfo;
+    Uri mTargetUri;
+
+    MediaPlayer mMediaPlayer;
+    SurfaceHolder mSurfaceHolder;
+    boolean mPaus = false;;
 
     @Bind(R.id.description)
     TextView mTextView;
+    @Bind(R.id.play_video_player)
+    Button mButtonPlayVideo;
+    @Bind(R.id.pause_video_player)
+    Button mButtonPauseVideo;
+    @Bind(R.id.surface_view)
+    SurfaceView mSurfaceView;
 
     public static VideoPreviewFragment newInstance(final int videoId) {
         VideoPreviewFragment videoPreviewFragment = new VideoPreviewFragment();
@@ -37,10 +58,48 @@ public class VideoPreviewFragment extends AbstractFragment implements LoaderMana
         return videoPreviewFragment;
     }
 
+    @OnClick(R.id.play_video_player)
+    public void onPlayVideo() {
+        mPaus = false;
+
+        if(mMediaPlayer.isPlaying()){
+            mMediaPlayer.reset();
+        }
+
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setDisplay(mSurfaceHolder);
+
+        try {
+            mMediaPlayer.setDataSource(getActivity().getApplicationContext(), mTargetUri);
+            mMediaPlayer.prepare();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mMediaPlayer.start();
+    }
+
+    @OnClick(R.id.pause_video_player)
+    public void onPauseVideo() {
+        if(mPaus){
+            mPaus = false;
+            mMediaPlayer.start();
+        }
+        else{
+            mPaus = true;
+            mMediaPlayer.pause();
+        }
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mVideoId = getArguments().getInt(EXTRA_VIDEO_ID);
+
     }
 
     @Nullable
@@ -53,6 +112,13 @@ public class VideoPreviewFragment extends AbstractFragment implements LoaderMana
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getLoaderManager().initLoader(R.id.video_preview_loader, VideoInfoLoader.prepareArgs(mVideoId), this);
+
+        getActivity().getWindow().setFormat(PixelFormat.UNKNOWN);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceHolder.setFixedSize(640, 480);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mMediaPlayer = new MediaPlayer();
     }
 
     @Override
@@ -63,6 +129,7 @@ public class VideoPreviewFragment extends AbstractFragment implements LoaderMana
     @Override
     public void onLoadFinished(Loader<VideoInfo> loader, VideoInfo data) {
         mVideoInfo = data;
+        mTargetUri = Uri.parse(mVideoInfo.getVideo());
         updateUi();
     }
 
@@ -77,5 +144,24 @@ public class VideoPreviewFragment extends AbstractFragment implements LoaderMana
             videoLibraryActivity.setToolbarTitle(mVideoInfo.getTitle());
         }
         mTextView.setText(mVideoInfo.getOverview());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMediaPlayer.release();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+
     }
 }
